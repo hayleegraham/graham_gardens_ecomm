558dd07dae15310067bdf64a8a137238b89737b1
hayleegraham <graham.haylee@yahoo.com>
2024-11-11 22:29:17 -0800

    implemented local storage

--- app/components/AppContext.js
+++ app/components/AppContext.js
@@ -1,51 +1,58 @@
-'use client'
+"use client";
 import React, { createContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
+  const initializeCartItems = () => {
+    if (typeof window !== "undefined") {
+      const storedCartItems = window.localStorage.getItem("cartItems");
+      return storedCartItems ? JSON.parse(storedCartItems) : [];
+    }
+    return [];
+  };
 
- const [cartItems, setCartItems] = useState([]);
- const [cartQty, setCartQty] = useState(0);
+  const [cartItems, setCartItems] = useState(initializeCartItems);
+  const [cartQty, setCartQty] = useState(0);
 
- //fires anytime cartItems is changed
- //calculates current cart qty
- useEffect(()=>{
-  let currQty = 0;
-  cartItems.forEach((item)=>{
-    currQty += item.qty;
-  })
-  setCartQty(currQty);
-  console.log("Cart Items:", cartItems)
- },[cartItems])
-  
+  //fires anytime cartItems is changed
+  //calculates current cart qty
+  useEffect(() => {
+    let currQty = 0;
+    cartItems.forEach((item) => {
+      currQty += item.qty;
+    });
+    setCartQty(currQty);
+    window.localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    console.log("Cart Items:", cartItems);
+  }, [cartItems]);
+
   const addProduct = (product, qty) => {
     //find returns first item in array that matches product name
-    const existingProduct = cartItems.find((item) => item.name === product.name);
-    if(existingProduct){
+    const existingProduct = cartItems.find(
+      (item) => item.name === product.name
+    );
+    if (existingProduct) {
       //if product is already in cart, only update qty
       const newQty = existingProduct.qty + qty;
-      setCartItems(prevCartItems => prevCartItems.map(item => 
-        item.name === product.name ? { ...item, qty: newQty } : item
-      ));
-    }else{
+      setCartItems((prevCartItems) =>
+        prevCartItems.map((item) =>
+          item.name === product.name ? { ...item, qty: newQty } : item
+        )
+      );
+    } else {
       //if not already in cart, add item to cart
       product.qty = qty;
-      setCartItems((prevCartItems) => [
-        ...prevCartItems, product
-        
-      ]);
+      setCartItems((prevCartItems) => [...prevCartItems, product]);
     }
-    
   };
-       
 
   return (
     <AppContext.Provider
       value={{
         addProduct,
         cartItems,
-        cartQty
+        cartQty,
       }}
     >
       {children}
